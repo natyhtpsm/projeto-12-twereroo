93294ebb8a63cf6192f045d642412eb6ced956dd
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-06-24 13:25:24 -0300

    Feat: post tweets

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 const user = [];
-const tweet = [];
+const tweets = [];
 
 app.get("/", (req, res) => {
     res.status(400).send("TEXTO");
@@ -23,5 +23,21 @@ app.post("/sign-up", (req, res) => {
     })
     return res.status(201).send("OK");
 });
+
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body;
+    if(!user.includes(username)){
+        return res.status(401).send("UNAUTHORIZED")
+    }
+    if(!tweet){
+        return res.status(400).send("Digite o tweet")
+    }
+    tweets.push({
+        username: username, 
+        tweet: tweet
+    })
+    return res.status(201).send("OK");
+})
+
 app.listen(5000, () => {console.log("Servidor funcionando na porta 5000")});
 
